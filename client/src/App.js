d310c2e887faef729c928cf779fa8e1bb46abc15
Nikstar-1 <rk_787@hotmail.co.uk>
2021-03-10 15:12:20 +0000

    new packages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState } from 'react';
+
+
+
 import './App.css';
 
 function App() {
+
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const [apiKey, setApiKey] = useState("AIzaSyAmukOX6JHM53iVJC7RJzMP-lGFQSiJ618");
+
+
+  function handleChange(event){
+    const book = event.target.value;
+    setBook(book);
+
+  }
+
+  function handleSubmit(event){
+     event.preventDefault();
+     axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + apiKey + "&maxResults=40")
+     .then(data => {
+       console.log(data.data.items);
+       setResult(data.data.items);
+     })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    <div className="container" >
+      <h1>Google Book Search </h1>
+      <form onSubmit={handleSubmit}> 
+        <div class="form-group">
+          <input 
+          type="text" onChange={handleChange}
+          className="form-control mt-10" 
+          placeholder="Search for books" 
+          autoComplete="off"
+          />
+          </div>
+          <button type="submit" className="btn btn-danger">Search</button>
+          <button type="submit" className="btn btn-danger">Saved</button>
+      </form>
+       {result.map(book => (
+         <a href={book.volumeInfo.previewLink}>
+         <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
+         </a>
+       )
+        )}
     </div>
+    
+  
   );
 }
+
 
 export default App;
