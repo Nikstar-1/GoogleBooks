e587bfa69358ce043c6dc42d0c074051ea01e432
Nikstar-1 <rk_787@hotmail.co.uk>
2021-03-13 22:04:43 +0000

    card display added for better browsing experience

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import API from "../utils/api";
+import CardDisplay from "../components/CardDisplay";
 
 
 // class Homepage extends Component {
- //  state = {
- //   books: [],
- //    search: "",
- // };
+//  state = {
+//   books: [],
+//    search: "",
+// };
 
-  //getSearchedBooks = () => {
-  //  API.getSearchedBooks(this.state.search).then((response) => this.setState({ books: response.data }));
-  // };
+//getSearchedBooks = () => {
+//  API.getSearchedBooks(this.state.search).then((response) => this.setState({ books: response.data }));
+// };
 //   render() {
 //     return (
 //       <div>
@@ -26,42 +27,49 @@ import { Link } from "react-router-dom";
 class Homepage extends Component {
   state = {
     books: [],
-     search: "",
+    search: "",
   };
-   handleChange = (event) => {
-      const book = event.target.value;
-      this.setState({...this.state, search: book});
-  
-    }
+  handleChange = (event) => {
+    const book = event.target.value;
+    this.setState({ ...this.state, search: book });
+  };
   getSearchedBooks = () => {
-    console.log(this.state)
+    console.log(this.state);
     API.getSearchedBooks(this.state.search).then((response) => this.setState({ books: response.data.items, search: "" }));
-   };
+  };
 
   render() {
     return (
-      <div className="card mb-3">
+      <div className="container-fluid">
         <nav>
           <Link to="/">Home</Link>
           <Link to="savedbooks">View Saved</Link>
         </nav>
-        <input
-          type="text" onChange={this.handleChange}
-          className="form-control mt-10"
-           placeholder="Search for books"
-          autoComplete="off"
-          
-           />
-         <button onClick={this.getSearchedBooks} type="submit" className="btn btn-info">Search</button>
-
-        {this.state.books.map(book => (
-         <a href={book.volumeInfo.previewLink}>
-          <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
-          </a>
-        )
-         )}
-    
+        <input type="text" onChange={this.handleChange} className="form-control mt-10" placeholder="Search for books" autoComplete="off" />
+        <button onClick={this.getSearchedBooks} type="submit" className="btn btn-info">
+          Search
+        </button>
+        
+        <CardDisplay 
+         id={book.volumeInfo.id} 
+         key={book.volumeInfo.id} 
+         title={book.volumeInfo.title} 
+         description={book.volumeInfo.description} 
+         image={book.volumeInfo.imageLinks.thumbnail} 
+         link={book.volumeInfo.infoLink} 
+         handleClick={this.handleBookClick}
+      
+        />
+        {this.state.books.map((book, i) => {
+          const key = i;
+          return (
+            <a href={book.volumeInfo.previewLink} key={key}>
+              <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} />
+            </a>
+          );
+        })}
       </div>
+      
     );
   }
 }
--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,17 +2,23 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom';
 
 
+
 class SavedBooks extends Component {
     state = {
-        books: []
+        books: [],
+        search: "",
       };
-      
+    
+
+
     render(){
+     
         return (
             <nav>
                 <Link to="/">Home</Link>
-                <Link to="savedbooks">View Saved</Link>
+                <Link to="savedbooks">View Saved</Link>    
             </nav>
+            
         )
     }
 }
